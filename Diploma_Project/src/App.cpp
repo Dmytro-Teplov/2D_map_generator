a4395aacc8e85601430965ea627b922ae1cbd477
Dmytro Teplov <daimon.tpl16@gmail.com>
2023-10-17 22:48:14 +0300

    Add project files.

---
+++ Diploma_Project/src/App.cpp
@@ -0,0 +1,210 @@
+
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct ShaderProgramSource
+{
+    std::string vertexShader;
+    std::string fragmentShader;
+};
+
+static ShaderProgramSource ParseShader(const std::string& source)
+{
+    std::ifstream stream(source);
+    std::string line;
+
+    int none = -1, vertex = 0, fragment = 1;
+    int type = none;
+
+    std::string shaders[2] = { "","" };
+
+    while (getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+            {
+                type = vertex;
+            }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                type = fragment;
+            }
+        }
+        else
+        {
+            shaders[type] += line + "\n";
+        }
+    }
+    return {shaders[vertex], shaders[fragment]};
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id,1,&src,nullptr);
+    glCompileShader(id);
+    //err
+    int result;
+
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH,&length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile shader!" << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error";
+    float positions[8] = {
+    -0.5f,  -0.5f,
+     0.5f,  -0.5f,
+     0.5f,  0.5f,
+    -0.5f,  0.5f
+    };
+
+    unsigned int indices[6] = {
+    0,1,2,
+    2,3,0
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1,&buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    
+    unsigned int indBuffer;
+    glGenBuffers(1, &indBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+
+    ShaderProgramSource sources;
+
+    sources = ParseShader("res/shaders/RedTris.shader");
+
+    unsigned int shader = CreateShader(sources.vertexShader, sources.fragmentShader);
+    glUseProgram(shader);
+
+    int location = glGetUniformLocation(shader, "u_Color");
+    //ASSERT(location != 1);
+    glUniform4f(location,0.5f,0.0f,0.5f,1.0f);
+    float col=0.0f;
+    /* Loop until the user closes the window */
+
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    float c = 1.0f;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        glUniform4f(location, c, c, c, 1.0f);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        
+       /* col += 0.05f;
+        if (col > 1.0f)
+        {
+            col = 0.0f;
+        }*/
+
+        ImGui::Begin("My name is window, ImGUI window");
+        ImGui::Text("He110 there adventurer! ");
+        ImGui::SliderFloat("Size", &c, 0.0f, 1.0f);
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glDeleteProgram(shader);
+
+    glfwTerminate();
+    return 0;
+}
